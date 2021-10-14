08441bbe0719d9bfdde1a57393bfd416cb88faa9
Stefan <sejsmont@creativeadvtech.com>
2021-10-14 23:55:33 +0200

    new logo square

--- okdd/src/components/Header.vue
+++ okdd/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header px-5 bg-white fixed z-50 top-0 w-full">
     <div class="max-w-screen-lg mx-auto flex">
-      <img class="mr-auto h-20 cursor-pointer" src="@/assets/logo-squere.png" alt="" @click="scrollToHash('hero')">
+      <LogoSquare class="mr-auto h-20 w-20 cursor-pointer"></LogoSquare>
       <div class="flex items-center ml-auto py-3">
         <div v-for="(item, index) in navItems" :key="index" class="navItem mr-16 uppercase text-lg" @click=scrollToHash(item.hash)>
           {{item.name}}
@@ -14,9 +14,13 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { NavItem } from '@/shared/interfaces/navItem.model'
+import LogoSquare from '@/components/icons/Logo-square.vue'
 
 export default defineComponent({
   name: 'Header',
+  components: {
+    LogoSquare
+  },
   props: {
     navItems: Object as PropType<NavItem[]>
   },
